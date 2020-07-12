d13021a0c2b50929e04f041c3689761409a3a8cc
Derek Hoogewerf <58857663+derek-hoogewerf@users.noreply.github.com>
2020-07-12 22:42:08 +0530

    Update perltut1.pl

--- lib/perltut1.pl
+++ lib/perltut1.pl
@@ -9,3 +9,6 @@ use v5.30;
 print "Hello World\n";
 my $name = 'Derek';
 my ($age, $street) = (20, '123 Main Street');
+my $my_info = "$name lives on \"$street\"\n";
+
+$my_info = qq{$name lives on "$street"\n};
