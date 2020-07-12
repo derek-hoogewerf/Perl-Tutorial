22411b1dc6a96cdaefc7acf9211e4b3aa1c6cb88
Derek Hoogewerf <58857663+derek-hoogewerf@users.noreply.github.com>
2020-07-12 23:41:36 +0530

    Update perltut1.pl

--- lib/perltut1.pl
+++ lib/perltut1.pl
@@ -33,10 +33,10 @@ printf ("%.16f \n", $big_float + $big_float);
 #math functions
 say "5 ** 4 = ", 5**4;
 
-say "EXP 1", exp 1;
-say "HEX 10", hex 10;
-say "OCT 10", oct 10;
-say " INT 6.45", int 6.45;
-say "LOG 2", log 2;
-say "Random number 0-10", int rand(11);
-say "SQRT 4", sqrt 4; 
+say "EXP 1 =", exp 1;
+say "HEX 10 =", hex 10;
+say "OCT 10 =", oct 10;
+say " INT 6.45 =", int 6.45;
+say "LOG 2 =", log 2;
+say "Random number 0-10 =", int rand(11);
+say "SQRT 4 =", sqrt 4; 
