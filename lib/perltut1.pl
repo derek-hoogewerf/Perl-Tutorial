fe94c3a96c9cee854642d015ce6de736ddf0a93d
Derek Hoogewerf <58857663+derek-hoogewerf@users.noreply.github.com>
2020-07-12 22:35:13 +0530

    Update perltut1.pl

--- lib/perltut1.pl
+++ lib/perltut1.pl
@@ -3,8 +3,9 @@ use warnings;
 use diagnostics;
 
 use feature 'say';
-use feature  'switch';
+use feature  'switch'; 
+use v5.30;
 
 print "Hello World\n";
 my $name = 'Derek';
-
+my ($age, $street) = (20, '123 Main Street');
