478896e5fa5982954a2b78985619f7d73f1f6765
Derek Hoogewerf <dbhoogewerf@gmail.com>
2020-08-10 13:30:54 +0530

    Update movies.pl

--- lib/movies.pl
+++ lib/movies.pl
@@ -11,6 +11,11 @@ while(<FH>)
     $entry = <FH>;
     $entry =~ s/dd-mm-yyyy hh:mm\s[0-9]{2}:[0-9]{2}\s(1| )(,| )[0-9]{3}//g;
     $entry =~ s/\s\- Shortcut.lnk//g;
-    #$entry =~ s/\.?\d{4}?\./\$2(\)/g;
+    if ($entry =~ m/(\.(mkv|ts|mp4|avi|flv|wmv)):(.*?s\([1900-yyyy]\))/)
+    {
+         my $format = $1;
+         my $title = $2;
+        $entry = $2+$1;
+    }
 }
 close(FH) || die "Couldn't close file properly";
