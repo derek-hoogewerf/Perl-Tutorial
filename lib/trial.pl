eafa476432d57de7b2cab37cb3945c01a34725f2
Derek Hoogewerf <dbhoogewerf@gmail.com>
2020-08-12 17:22:59 +0530

    Update trial.pl

--- lib/trial.pl
+++ lib/trial.pl
@@ -1,22 +1,42 @@
 use URI;
 use Web::Scraper;
- 
-# First, create your scraper block
-my $movie = scraper {
-    # Parse all LIs with the class "status", store them into a resulting
-    # array 'tweets'.  We embed another scraper for each movie's details.
-    process "title_bar_wrapper", "movie_details[]" => scraper {
-        # And, in that array, pull in the elementy with the class
-        # "entry-content", "entry-date" and the link
-        process ".title_wrapper > title_bar > h1", title => 'TEXT';
-        process ".title_wrapper > h1 > span", link => '@href', when => 'TEXT'; 
+use Encode;
+use warnings;
+use strict;
+use Data::Dumper;
+use diagnostics;
+use LWP::Simple qw(get getstore); 
+use JSON;
+binmode STDOUT, ":utf8";
+use utf8;
+
+=pod
+my $para = scraper {
+        process 'div p', "para[]" => scraper {
+        # get text inside "small" element
+        process 'p', each_para => 'TEXT';
     };
 };
  
-my $res = $movie->scrape( URI->new("https://www.imdb.com/title/tt1413492/?ref_=nv_sr_srsg_0") );
+my $res = $para->scrape( URI->new("http://qdnet.com/training/example.html") );
  
-# The result has the populated details array
-for my $movie (@{$res->{movie_details}}) {
-    print "$movie->{title} $movie->{when} (link: $movie->{link})\n";
+# iterate the array 'authors'
+for my $author (@{$res->{para}}) {
+    print Encode::encode("utf8", "$author->{each_para}\n");
+}
+=cut
+my $file = "movies_1.json";
+my $api = "https://imdb-api.com/en/API/SearchMovie/k_1FB31LF3/Inception%202010";
+my $api_content = getstore($api,$file);
+
+my $json;
+{
+  local $/; #Enable 'slurp' mode
+  open my $fh, "<", "movies_1.json";
+  $json = <$fh>;
+  close $fh;
 }
 
+my $data = decode_json($json);
+#print Dumper($data);
+print $data->{'results'}->[0]->{'title'};
